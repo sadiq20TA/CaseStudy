2b9c1def577638ee1a17e4f8da93ad3e8e5cab2d
sadiq20TA <mohammed.sadiq@tigeranalytics.com>
2022-05-16 15:09:27 +0530

    till creation of train, test set

---
+++ notebooks/reference - Copy/scripts.py
@@ -0,0 +1,7 @@
+"""Module for listing down additional custom functions required for the notebooks."""
+
+import pandas as pd
+
+def binned_selling_price(df):
+    """Bin the selling price column using quantiles."""
+    return pd.qcut(df["sales_dollars_value"], q=10)
